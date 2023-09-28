4b9d7786ef85375df5859a983af87ac649c0c69d
A01661436 <A01661436@tec.mx>
2023-09-27 18:02:05 -0600

    Initial Commit

---
+++ Actividades/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Actividades
+//
+//  Created by Diego Martell on 25/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
