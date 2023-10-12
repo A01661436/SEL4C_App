9ab8ab2c934812c2a205768905672312022ae502
adri-agsz <adrian.agsz10@gmail.com>
2023-10-11 18:46:36 -0600

    checkbox images

--- Actividades/Managers/RegistroManager.swift
+++ Actividades/Managers/RegistroManager.swift
@@ -11,7 +11,7 @@ class RegistroManager {
 
     public func registraUsuario(with datosUsuario: [String: Any], completion: @escaping ((Bool)-> Void))
     {
-        let urlRegistro = URL(string: "http://18.222.144.45:8000/api/api/users/")!
+        let urlRegistro = URL(string: "http://18.222.144.45:8000/api/users/")!
         
         // Convertir los datos del usuario a JSON
         guard let jsonData = try? JSONSerialization.data(withJSONObject: datosUsuario, options: []) else {
